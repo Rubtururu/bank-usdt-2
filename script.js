a716b6f964aa655f01068516e686068e9732d4a3
Rubtururu <59855904+Rubtururu@users.noreply.github.com>
2024-02-25 21:05:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', async () => {
+    document.addEventListener('DOMContentLoaded', async () => {
     if (window.ethereum) {
         window.web3 = new Web3(window.ethereum);
         await window.ethereum.enable();
 
-        const contractAddress = '0x2De63496813f1CaDAB2840131F095eb17e54ECb1';
+        const contractAddress = '0x2De63496813f1CaDAB2840131F095eb17e54ECb1'; // Reemplaza con la dirección del contrato desplegado
         const contractABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"ClaimDividends","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Withdrawal","type":"event"},{"inputs":[],"name":"ceoAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"claimDividends","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"deposit","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"getContractBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"lastDividendsPaymentTime","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalDeposits","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalDividendsPool","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalTreasuryPool","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"userDeposits","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"userWithdrawals","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}];
 
         const contract = new web3.eth.Contract(contractABI, contractAddress);
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         document.getElementById('withdraw-form').addEventListener('submit', async (e) => {
             e.preventDefault();
             const amount = document.getElementById('withdraw-amount').value;
-            await contract.methods.withdraw(web3.utils.toWei(amount, 'ether')).send({ from: userAccount });
+            await contract.methods.withdraw().send({ from: userAccount });
             updateStats();
             document.getElementById('withdraw-amount').value = ''; // Limpiar el campo después del retiro
         });
@@ -38,23 +38,24 @@ document.addEventListener('DOMContentLoaded', async () => {
         // Función para actualizar las estadísticas del contrato
         async function updateStats() {
             const ceoAddress = await contract.methods.ceoAddress().call();
-            const ceoAddress2 = await contract.methods.ceoAddress2().call();
-            const lastDividendsPaymentTime = await contract.methods.lastDividendsPaymentTime().call();
-            const deposits = await contract.methods.deposits(userAccount).call();
-            const contractBalance = await contract.methods.contractBalance().call();
-            const timeSinceLastDeposit = await contract.methods.timeSinceLastDeposit().call();
             const totalDeposits = await contract.methods.totalDeposits().call();
+            const totalTreasuryPool = await contract.methods.totalTreasuryPool().call();
+            const totalDividendsPool = await contract.methods.totalDividendsPool().call();
+            const lastDividendsPaymentTime = await contract.methods.lastDividendsPaymentTime().call();
+            const userDeposits = await contract.methods.userDeposits(userAccount).call();
+            const userWithdrawals = await contract.methods.userWithdrawals(userAccount).call();
+            const contractBalance = await contract.methods.getContractBalance().call();
 
             document.getElementById('ceo-address').innerText = ceoAddress;
-            document.getElementById('ceo-address2').innerText = ceoAddress2;
+            document.getElementById('total-deposits').innerText = web3.utils.fromWei(totalDeposits, 'ether');
+            document.getElementById('total-treasury-pool').innerText = web3.utils.fromWei(totalTreasuryPool, 'ether');
+            document.getElementById('total-dividends-pool').innerText = web3.utils.fromWei(totalDividendsPool, 'ether');
             document.getElementById('last-dividends-payment-time').innerText = new Date(lastDividendsPaymentTime * 1000).toLocaleString();
-            document.getElementById('user-deposits').innerText = web3.utils.fromWei(deposits, 'ether');
+            document.getElementById('user-deposits').innerText = web3.utils.fromWei(userDeposits, 'ether');
+            document.getElementById('user-withdrawals').innerText = web3.utils.fromWei(userWithdrawals, 'ether');
             document.getElementById('contract-balance').innerText = web3.utils.fromWei(contractBalance, 'ether');
-            document.getElementById('time-since-last-deposit').innerText = timeSinceLastDeposit;
-            document.getElementById('total-deposits').innerText = totalDeposits;
         }
     } else {
         alert('Por favor, instala MetaMask para utilizar esta aplicación.');
     }
 });
-
